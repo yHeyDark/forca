f9442fa58b7784c917b6228ce6de2b4642dc3f22
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-03-31 19:16:58 -0300

    add Forca

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string letra, forca;
+bool exite;
+
+Console.Write("Qual a frase secreta: ");
+forca = Console.ReadLine()!;
+Console.WriteLine("Escreva a Letra: ");
+letra = Console.ReadLine()!;
+
+exite = forca.Contains(letra.ToLower());
+
+Console.Write($"A letra é {exite}");
+
